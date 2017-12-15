82eb4a6ac4757bd9cf39f2cee2736b6ed586d32f
Nuno Santos <nunofsantos@gmail.com>
2017-12-14 22:01:09 -0500

    Self-calibration for VibrationSensor

--- raspberrypi_utils/input_devices.py
+++ raspberrypi_utils/input_devices.py
@@ -1,10 +1,15 @@
 from abc import ABCMeta, abstractmethod
+import logging
+from operator import add, div
 from threading import Thread
 from time import sleep
 
 from LIS3DH import LIS3DH
 import RPi.GPIO as GPIO
 from yunomi import Meter
+
+
+log = logging.getLogger(__name__)
 
 
 class ThreadedDigitalInputDevice(object):
@@ -54,22 +59,51 @@ class Button(ThreadedDigitalInputDevice):
 
 class VibrationSensor(ThreadedDigitalInputDevice):
     def __init__(self, address=0x18, bus=1, frequency_seconds=1,
-                 sensitivity=(1, 0.1, 0.1), threshold_per_minute=1,
-                 vibration_callback=None, steady_vibration_callback=None):
+                 sensitivity=(0.05, 0.05, 0.05), auto_calibrate=True, auto_sensitivity=1.0,
+                 threshold_per_minute=1, vibration_callback=None, steady_vibration_callback=None):
         GPIO.setmode(GPIO.BCM)
         self.meter = Meter()
         self.frequency_seconds = frequency_seconds
         self.sensitivity = sensitivity
+        self.auto_calibrate = auto_calibrate
+        self.auto_sensitivity = auto_sensitivity
         self.threshold_per_minute = threshold_per_minute
+        self.calibration = (0.0, 0.0, 0.0)
         self.sensor = LIS3DH(address=address, bus=bus)
+        if self.auto_calibrate:
+            self._calibrate()
         super(VibrationSensor, self).__init__(
             vibration_callback,
             steady_vibration_callback
         )
 
+    def _calibrate(self, iterations=50):
+        # figure out which axis is measuring gravity, and calibrate accordingly to ignore its effect
+        totals = [0, 0, 0]
+        for i in xrange(iterations):
+            readings = [abs(self.sensor.get_x()), abs(self.sensor.get_y()), abs(self.sensor.get_z())]
+            totals = map(add, readings, totals)
+            sleep(0.2)
+        self.calibration = [round(x, 3) for x in map(div, totals, [iterations]*3)]
+        log.debug('VibrationSensor: self calibration (x, y, z) = {}'.format(self.calibration))
+        self.sensitivity = []
+        for x in self.calibration:
+            if round(x, 0) == 1:
+                # gravity axis, remove gravity factor before multiplying, then add back in
+                x %= 1
+                x *= 1.0 + self.auto_sensitivity
+                x += 1.0
+            else:
+                x *= 1.0 + self.auto_sensitivity
+            self.sensitivity.append(round(x, 3))
+        log.debug('VibrationSensor: calculated sensitivity (x, y, z) = {}'.format(self.sensitivity))
+
     def read(self):
         rate = self.meter.get_one_minute_rate()
         if rate > self.threshold_per_minute:
+            log.debug('VibrationSensor: rate {:.2f} above threshold {:.2f}, steady callback'.format(
+                rate, self.threshold_per_minute)
+            )
             self.notify_threshold(rate)
         return rate
 
@@ -78,9 +112,11 @@ class VibrationSensor(ThreadedDigitalInputDevice):
 
     def run(self):
         while True:
-            readings = (self.sensor.get_x(), self.sensor.get_y(), self.sensor.get_z())
-            if any(True for i in xrange(3) if abs(readings[i]) > self.sensitivity[i]):
+            readings = [abs(round(x, 3)) for x in (self.sensor.get_x(), self.sensor.get_y(), self.sensor.get_z())]
+            log.debug('VibrationSensor: readings (x, y, z) = {}'.format(readings))
+            if any(True for i in xrange(3) if abs(readings[i] - self.calibration[i]) > self.sensitivity[i]):
                 self.meter.mark()
+                log.debug('VibrationSensor meter marked')
                 self.notify_immediate(True)
             else:
                 self.notify_immediate(False)
